602ec4a056cee349da1266d56f4c6302b80f9ba9
atfreij <atfreij@ncsu.edu>
2020-04-22 15:41:34 -0400

    added another example

--- ptr-practice.c
+++ ptr-practice.c
@@ -5,20 +5,31 @@ int main() {
   
   int *stkptr;
   int a;
+  int b;
 
   a = 25;
   stkptr = &a;
   
   printf("Welcome to ptr test!\n");
-  printf("a: %d\n", a);
-  printf("a address: %p\n", (void *)&a);
+  printf("a value: %d\n", a);
+  printf("&a (address-of a): %p\n", (void*)&a);
   printf("stkptr value: %p\n", stkptr);
-  printf("stkptr address: %p\n", (void *)&stkptr);
-  printf("*stkptr: %d\n", *stkptr);
+  printf("&stkptr (address-of stkptr): %p\n", (void*)&stkptr);
+  printf("*stkptr (dereference stkptr): %d\n", *stkptr);
 
   *stkptr = 50;
   printf("*stkptr (re-written): %d\n", *stkptr);
   printf("stkptr (re-written): %p\n", stkptr);
+
+  // Equivalence tests
+  b = 100;
+  *stkptr = b;
+  printf("stkptr value: %p\n", stkptr);
+  printf("&stkptr (address-of stkptr): %p\n", (void*)&stkptr); 
+  printf("*stkptr (dereference stkptr): %d\n", *stkptr);
+  printf("b value: %d\n", b);
+  printf("&b (address-of b): %p\n", &b);
+  printf("a value: %d\n", a);
 
   return 0;
 
